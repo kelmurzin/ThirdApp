8c65d0787fc2d7d22784607a01d1bd14552f00e0
kelmurzin <temikboyz@yandex.ru>
2022-02-09 15:34:02 +0400

    fix

---
+++ Assets/Script/ChoiceObject.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChoiceObject : MonoBehaviour
+{
+    [SerializeField] private ColorChange _сolorChange;
+
+    private GameObject _objectSet;
+    private Image _image;
+
+    private Color32 _color;
+    
+    public void ColorGet(Color32 color)
+    {
+        _color = color;
+
+        if (_objectSet != null)
+            _objectSet.GetComponent<MeshRenderer>().material.color = _color;
+        
+        if (_image != null)
+            _image.GetComponent<Image>().color = _color;        
+    }
+
+    public void SetObject(GameObject obj)
+    {
+        _image = null;
+        _objectSet = obj;
+        ChangeMat();        
+    }
+
+    public void SetImage(Image im)
+    {
+        _objectSet = null;
+        _image = im;
+        ChangeColor();        
+    }
+
+    private void OnEnable()
+    {
+        _сolorChange.ColorChanged += ColorGet;
+    }
+
+    private void ChangeMat()
+    {
+        _color = _objectSet.GetComponent<MeshRenderer>().material.color;
+        _сolorChange.ColorSet(_color);
+    }
+
+    private void ChangeColor()
+    {
+        _color = _image.GetComponent<Image>().color;
+        _сolorChange.ColorSet(_color);
+    }
+
+    private void OnDisable()
+    {
+        _сolorChange.ColorChanged -= ColorGet;
+    }
+    
+}
---
+++ Assets/Script/MoveImage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class MoveImage : MonoBehaviour
+{
+    [SerializeField] private Image _image;
+
+    private void Start() => _image.rectTransform.DOLocalMoveX((758), 2f)
+        .SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+}
